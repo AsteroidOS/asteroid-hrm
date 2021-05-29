0507ca83701867ac362d6c14b910da8da5b0abcf
Bart Ribbers <bribbers@disroot.org>
2021-05-29 18:08:13 +0200

    Show a text indicating it's still measuring rather than 0 bpm

---
+++ i18n/asteroid-hrm.en_GB.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name></name>
+    <message id="id-measuring">
+        <location filename="../src/main.qml" line="40" />
+        <source>Measuring...</source>
+        <translation>Measuring...</translation>
+    </message>
+</context>
+</TS>
