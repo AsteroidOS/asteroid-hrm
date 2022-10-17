26dc3a558d77c80743a8e74d0a935582cd157110
Enol P <enolp@softastur.org>
2022-10-17 23:09:43 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-hrm.ast.ts
+++ i18n/asteroid-hrm.ast.ts
@@ -6,12 +6,12 @@
     <message id="id-measuring">
         <location filename="../src/main.qml" line="133"/>
         <source>Measuring...</source>
-        <translation type="unfinished"></translation>
+        <translation>Midiendo…</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-hrm.desktop.h" line="6"/>
         <source>Heart Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>Frecuencia cardiaca</translation>
     </message>
 </context>
 </TS>
