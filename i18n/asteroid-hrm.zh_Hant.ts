1d26f8799c948983f5e4fb995c60527abbb83982
hugoalh <hugoalh@users.noreply.hosted.weblate.org>
2024-05-14 09:28:49 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (2 of 2 strings)

--- i18n/asteroid-hrm.zh_Hant.ts
+++ i18n/asteroid-hrm.zh_Hant.ts
@@ -6,12 +6,12 @@
     <message id="id-measuring">
         <location filename="../src/main.qml" line="133"/>
         <source>Measuring...</source>
-        <translation type="unfinished"></translation>
+        <translation>正在測量…</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-hrm.desktop.h" line="6"/>
         <source>Heart Rate</source>
-        <translation type="unfinished"></translation>
+        <translation>心率</translation>
     </message>
 </context>
 </TS>
