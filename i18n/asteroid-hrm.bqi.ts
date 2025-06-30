f192940ea9878371085b50c2fbed741adaae07b6
hosêyň abāspanā <hosseinabaspanah@gmail.com>
2025-06-30 06:44:18 +0200

    Added translation using Weblate (Luri (Bakhtiari))

---
+++ i18n/asteroid-hrm.bqi.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bqi">
+<context>
+    <name></name>
+    <message id="id-measuring">
+        <location filename="../src/main.qml" line="133"/>
+        <source>Measuring...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-hrm.desktop.h" line="6"/>
+        <source>Heart Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
